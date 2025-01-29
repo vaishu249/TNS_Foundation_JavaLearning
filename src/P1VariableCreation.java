f883aab25c2f2866e412248f7bef489125d633b4
vaishu1109 <vaishu.sg1109@gmail.com>
2025-01-29 14:37:14 +0530

    variable declartion

--- src/P1VariableCreation.java
+++ src/P1VariableCreation.java
@@ -1,5 +1,22 @@
 public class P1VariableCreation {
     public static void main(String[] args) {
+        int a=20;
+        long l=12L;
+        String name="Vaishnavi";
+        float _marks=95.5f;
+        char c='a';
+        double d=1211111d;
+        boolean value=true;
+        byte b='a';
+        System.out.println("integer:"+a);
+        System.out.println("Long:"+l);
+        System.out.println("string : "+name);
+        System.out.println("Float : "+_marks);
+        System.out.println("Double : "+d);
+        System.out.println("Boolean : "+value);
+        System.out.println("Character : "+c);
+        System.out.println("Byte : "+b);
+
 
     }
 }
