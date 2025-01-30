fbe598c4e06518c520cdc3610279827bc8ab9acb
vaishu1109 <vaishu.sg1109@gmail.com>
2025-01-30 16:01:31 +0530

    basic if else code

---
+++ src/P3ControlStatement/BasicIfElse.java
@@ -0,0 +1,20 @@
+package P3ControlStatement;
+
+public class BasicIfElse {
+    public static void main(String[] args) {
+        int x=5;
+        int y=10;
+        if(x==5){
+            System.out.println("The no is equal to 5");
+        }
+        else{
+            System.out.println("No is not equal to 5 ");
+        }
+        if(x>y){
+            System.out.println(x+" is greater ");
+        }
+        else {
+            System.out.println(y+" is greater ");
+        }
+    }
+}
