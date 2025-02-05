4bb9056daf25a9cb007863d00ea4947caff03e26
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-05 14:36:58 +0530

    Do while loop

---
+++ src/P9DoWhileLoop.java
@@ -0,0 +1,9 @@
+public class P9DoWhileLoop {
+    public static void main(String[] args) {
+        int a=95;
+        do{
+            System.out.println(a);
+            a++;
+        }while(a<90);
+    }
+}
