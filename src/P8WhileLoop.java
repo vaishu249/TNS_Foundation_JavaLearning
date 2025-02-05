8f9656b0ac5284e47c3169e9662d374b0382da0b
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-05 14:34:43 +0530

    while loop

---
+++ src/P8WhileLoop.java
@@ -0,0 +1,9 @@
+public class P8WhileLoop {
+    public static void main(String[] args) {
+        int i = 95;
+        while(i<=100){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
