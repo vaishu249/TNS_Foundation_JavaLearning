a51f15453d9691fe534eeb4905ee59a0c04fd606
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-05 15:03:12 +0530

    Break in java

---
+++ src/P13Break.java
@@ -0,0 +1,11 @@
+public class P13Break {
+    public static void main(String[] args) {
+        int i=10;
+        for (int j = i; j <20 ; j++) {
+            if(j==15){
+                break;
+            }
+            System.out.println(j);
+        }
+    }
+}
