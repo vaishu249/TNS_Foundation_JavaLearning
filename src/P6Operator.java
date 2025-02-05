c791c0d1ebf453bff98902f8d71c22c14cb8a8bd
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-05 14:37:10 +0530

    operator increment

--- src/P6Operator.java
+++ src/P6Operator.java
@@ -2,8 +2,9 @@ public class P6Operator {
     public static void main(String[] args) {
         int a=9;
         int b=10;
-        int c=a++ + ++b;
+        int c=a++ +a+ ++b;
         System.out.println(c);
-
+        int d=a++ +c+ ++b;
+        System.out.println(d);
     }
 }
