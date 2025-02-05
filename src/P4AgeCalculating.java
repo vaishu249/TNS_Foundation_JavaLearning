4e8838cb7a891a76aea52e3f398ca95b1a1e938b
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-05 14:12:12 +0530

    basic if else code related to age

---
+++ src/P4AgeCalculating.java
@@ -0,0 +1,11 @@
+public class P4AgeCalculating {
+    public static void main(String[] args) {
+        int age=8;
+        if(age<18){
+            System.out.println("Adult");
+        }
+        else{
+            System.out.println("minor");
+        }
+    }
+}
