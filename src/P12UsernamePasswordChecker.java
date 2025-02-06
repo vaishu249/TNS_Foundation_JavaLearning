f671322ed200cfce4d8c112a764314e5b952fb21
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-06 14:09:24 +0530

    Password and username

--- src/P12UsernamePasswordChecker.java
+++ src/P12UsernamePasswordChecker.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class P12UsernamePasswordChecker {
     public static void main(String[] args) {
-        long password=123456789;
+        String userName = "Vaishnavi";
+        String password = "vaishu@2409";
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter User Name: ");
+        String uName = input.nextLine();
+        System.out.print("Enter Your Password: ");
+        String pass = input.nextLine();
+
+        if(uName.equals(userName) && pass.equals(password)){
+            System.out.println("Welcome User");
+        } else if (uName.equals(userName) && !pass.equals(password)) {
+            System.out.println("Invalid Password");
+        } else if (!uName.equals(userName) && pass.equals(password)) {
+            System.out.println("Invalid User-Name");
+        }
+        else if (!uName.equals(userName) && !pass.equals(password)) {
+            System.out.println("Invalid Credentials");
+        }
     }
 }
