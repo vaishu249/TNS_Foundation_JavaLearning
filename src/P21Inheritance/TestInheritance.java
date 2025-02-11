58e5216a46fbd3d3f0fed9a9ef97bfc2d18aace1
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-11 22:18:06 +0530

    person for inheritance

---
+++ src/P21Inheritance/TestInheritance.java
@@ -0,0 +1,8 @@
+package P21Inheritance;
+
+public class TestInheritance {
+    public static void main(String[] args) {
+        Student s1=new Student("Vaishnavi",1234567890,"Pune",5,"Keystone");
+        System.out.println(s1);
+    }
+}
