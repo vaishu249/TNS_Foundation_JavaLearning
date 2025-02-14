14a7629433030346699f1afd46dabbb31bbc8827
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-14 15:06:51 +0530

    hierarchy inheritance

--- src/P22HierarchicalInheritance/Person.java
+++ src/P22HierarchicalInheritance/Person.java
@@ -1,7 +1,41 @@
 package P22HierarchicalInheritance;
 
 public class Person {
-    public static void main(String[] args) {
+    private String name ;
+    private String city;
 
+    public Person() {
+        System.out.println("Default constructor ....");
+        name="kunal";
+        city="Kharadi";
+    }
+
+    public Person(String city, String name) {
+        this.city = city;
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "city='" + city + '\'' +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
