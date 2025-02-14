abcd5f26dbb02281d026549bebb2286f3cadd487
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-14 15:07:26 +0530

    static keyword

---
+++ src/P24StaticVariableWithStaticValue/TestEmployee.java
@@ -0,0 +1,8 @@
+package P24StaticVariableWithStaticValue;
+
+public class TestEmployee {
+    public static void main(String[] args) {
+        Employee e=new Employee("Vaishnavi",1);
+        System.out.println(e);
+    }
+}
