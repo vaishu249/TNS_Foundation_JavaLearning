1170adac19c9e79a74772983a217851d1e577bc4
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-17 15:04:17 +0530

    test constructor Overloading

---
+++ src/P26Overloading/TestConstructor.java
@@ -0,0 +1,13 @@
+package P26Overloading;
+
+public class TestConstructor {
+    public static void main(String[] args) {
+        System.out.println("Constructor Overloading ");
+        ConstructorOverloading c=new ConstructorOverloading();
+        System.out.println(c);
+        ConstructorOverloading c2=new ConstructorOverloading(12.5f);
+        System.out.println(c2);
+        ConstructorOverloading c3=new ConstructorOverloading(1.1f,2.5f);
+        System.out.println(c3);
+    }
+}
