31a2bd1b085b2be3863bf10f2827869f226e99f0
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-18 09:19:32 +0530

    changes

--- src/P27Task_1/OverrideMethodOfPayment.java
+++ src/P27Task_1/OverrideMethodOfPayment.java
@@ -2,15 +2,15 @@ package P27Task_1;
 
 public class OverrideMethodOfPayment {
     public void Display(int amount,long CardNumber,String CardHolder){
-        System.out.println("Payment Successfully...");
+        System.out.println("\n---------------Payment Successfully--------------------\n");
     }
     public void Display(int amount,int userId,String BankName,String Password){
-        System.out.println("Payment Successfully...");
+        System.out.println("\n---------------Payment Successfully--------------------\n");
     }
     public void Display(int amount,String QRCode,int Pin){
-        System.out.println("Payment Successfully...");
+        System.out.println("\n---------------Payment Successfully--------------------\n");
     }
     public void Display(int amount,long PhoneNo,int pin){
-        System.out.println("Payment Successfully...");
+        System.out.println("\n---------------Payment Successfully--------------------\n");
     }
 }
