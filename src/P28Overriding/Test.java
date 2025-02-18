e5daedd37bd08a60bd77565699a1c5e6833b621d
vaishu1109 <vaishu.sg1109@gmail.com>
2025-02-18 10:53:23 +0530

    Method Overriding Test

---
+++ src/P28Overriding/Test.java
@@ -0,0 +1,19 @@
+package P28Overriding;
+
+public class Test {
+    public static void main(String[] args) {
+        RBI_Parent r;
+        //Dynamic
+        r=new RBI_Parent();
+        System.out.println(r.getRateOfIntrest());
+
+        r=new HDFC_Child();
+        System.out.println(r.getRateOfIntrest());
+
+        r=new SBIChildClass();
+        System.out.println(r.getRateOfIntrest());
+
+        r=new ICIC_Child();
+        System.out.println(r.getRateOfIntrest());
+    }
+}
