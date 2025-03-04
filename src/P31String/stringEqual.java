4ec045cd160c0da46aa6d5c7c8fd6e8d36c7c1b7
vaishu1109 <vaishu.sg1109@gmail.com>
2025-03-04 11:40:25 +0530

    string equals

---
+++ src/P31String/stringEqual.java
@@ -0,0 +1,9 @@
+package P31String;
+
+public class stringEqual {
+    public static void main(String[] args) {
+        String s1="vai";
+        String s2="vai";
+        s1.compareTo(s2);
+    }
+}
