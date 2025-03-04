39f4919041bc244200864fb2e00028917e5c4743
vaishu1109 <vaishu.sg1109@gmail.com>
2025-03-04 11:39:59 +0530

    string buffer

---
+++ src/P32StringBuffer/StringBufferExample.java
@@ -0,0 +1,11 @@
+package P32StringBuffer;
+
+public class StringBufferExample {
+    public static void main(String[] args) {
+        StringBuffer buffer=new StringBuffer("Hello");
+        System.out.println(buffer);
+
+        buffer=new StringBuffer(40);
+        System.out.println(buffer);
+    }
+}
