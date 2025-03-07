42b271ccd0eba7ac5a2006623c1baa8f226d742f
vaishu1109 <vaishu.sg1109@gmail.com>
2025-03-07 23:08:57 +0530

    product class

---
+++ src/CoreJavaProject/entities/Product.java
@@ -0,0 +1,48 @@
+package CoreJavaProject.entities;
+
+public class Product {
+    private int productId;
+    private String name;
+    private double price;
+    private int stockQuantity;
+
+    public Product(int productId, String name, double price, int stockQuantity) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+}
+
