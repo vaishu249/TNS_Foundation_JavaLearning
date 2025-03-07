2046a0001afa249f9b8f94f75bc32bd2be899215
vaishu1109 <vaishu.sg1109@gmail.com>
2025-03-07 23:07:26 +0530

    admin class

---
+++ src/CoreJavaProject/entities/Admin.java
@@ -0,0 +1,7 @@
+package CoreJavaProject.entities;
+
+public class Admin extends User {
+    public Admin(int userId, String username, String email) {
+        super(userId, username, email);
+    }
+}
